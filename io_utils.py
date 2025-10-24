560e031b34edb2667708ece3c15af12efd1bd884
EnzoGCastro <enzoguerrieridecastro@gmail.com>
2025-10-23 23:30:08 -0400

    Add save and load functionality

--- io_utils.py
+++ io_utils.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+import json
+
+saved_data_path = Path(__file__).parent
+settings_path = saved_data_path/"report_settings.json"
+
 def get_student_info():
 	student_list = []
 	student_num = int(input("Insert num of students: "))
@@ -19,9 +25,29 @@ def get_questions():
 		
 	return (question_list, question_num)
 
-def get_lab_info():
+def get_lab_parent_path_str():
 	lab_path_string = input("Insert absolute path to folder which contains all lab folders: ")
+	return lab_path_string
+
+def get_lab_num():
 	lab_num = int(input("Insert lab num: "))
+	return lab_num
+
+def save_data(parent_path, student_list):
 	
-	return (lab_path_string,lab_num)
+	data = {
+		"parent_path": str(parent_path),
+		"student_num": len(student_list),
+		"student_list": student_list
+	}
 	
+	with open(settings_path, "w") as f:
+		json.dump(data, f, indent=4)
+
+def load_data():
+	if settings_path.exists():
+		with open(settings_path, "r") as f:
+			data = json.load(f)
+			data["parent_path"] = Path(data["parent_path"])
+			return data
+	return None
--- reportautomator.py
+++ reportautomator.py
@@ -1,22 +1,39 @@
 from file_utils import make_parent_path, make_lab_path, make_output_path, list_c_files_and_code
-from io_utils import get_lab_info, get_student_info, get_questions
+from io_utils import get_lab_parent_path_str, get_lab_num, get_student_info, get_questions, save_data, load_data
 from report_generator import write_report
 from pathlib import Path
 
 def main():
-	student_list = get_student_info()
-	questions_list, question_num = get_questions()
-	parent_string, lab_num = get_lab_info()
 	
-	parent_path = make_parent_path(parent_string)
+	load_result = load_data()
+	
+	if load_result:
+		student_list = load_result["student_list"]
+		student_num = load_result["student_num"]
+		parent_path = load_result["parent_path"]
+	else:
+		#Get lab information
+		student_list = get_student_info()
+		parent_string = get_lab_parent_path_str()
+		
+	lab_num = get_lab_num()
+	questions_list, question_num = get_questions()
+	
+	
+	#Build filepaths
+	if not load_result:
+		parent_path = make_parent_path(parent_string)
 	student_names_list = [student[0] for student in student_list]
 	lab_path = make_lab_path(parent_path, lab_num)
 	output_path = make_output_path(student_names_list, lab_num, lab_path)
+	
+	#Get c files and code
 	c_files_and_code = list_c_files_and_code(lab_path)
 	
+	#Write the report
 	write_report(output_path, questions_list, c_files_and_code, student_list)
 	
-	
+	save_data(parent_path, student_list)
 
 if __name__ == "__main__":
     main()
